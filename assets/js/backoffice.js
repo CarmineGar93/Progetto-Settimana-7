a63224334b729e48ee35cf8e9b6ded7c051a3e49
Carmy93 <carmygargiulo45@gmail.com>
2024-07-26 16:40:03 +0200

    commit

---
+++ assets/js/backoffice.js
@@ -0,0 +1,195 @@
+const btnLoad = document.getElementById("load");
+const btnModify = document.getElementById("modify");
+const btnDelete = document.getElementById("delete");
+const btnSelect = document.getElementById("btnSelect");
+const carousel = document.getElementById("carousel");
+const URL = "https://striveschool-api.herokuapp.com/api/product/";
+const nameInput = document.getElementById("name");
+const descriptionInput = document.getElementById("description");
+const priceInput = document.getElementById("price");
+const brandInput = document.getElementById("brand");
+const imgUrlInput = document.getElementById("imageUrl");
+
+class myProduct {
+    constructor(_name, _description, _brand, _imageUrl, _price) {
+        this.name = _name;
+        this.description = _description;
+        this.brand = _brand;
+        this.imageUrl = _imageUrl;
+        this.price = _price;
+    }
+}
+const productForm = document.getElementById("productForm");
+productForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const newProduct = new myProduct(
+        nameInput.value,
+        descriptionInput.value,
+        brandInput.value,
+        imgUrlInput.value,
+        priceInput.value
+    );
+    fetch(URL, {
+        method: "POST",
+        body: JSON.stringify(newProduct),
+        headers: {
+            "Content-Type": "application/json",
+            Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNTcwOGYyNjBjYzAwMTVjYzBkZDYiLCJpYXQiOjE3MjE5ODA2ODAsImV4cCI6MTcyMzE5MDI4MH0.BM-Bffn7x4mrYJT06TzOTGpAjF0ItIO21j7f9WF574Q",
+        },
+    })
+        .then((response) => {
+            if (response.ok) {
+                alert("PRODUCT SAVED");
+            } else {
+                alert("OPS...SOMETHING WENT WRONG TRY AGAIN");
+                throw new Error("Errore nel salvataggio del concerto");
+            }
+        })
+        .catch((err) => {
+            console.log("ERRORE", err);
+        });
+});
+
+const modal = function () {
+    fetch(URL, {
+        headers: {
+            Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNTcwOGYyNjBjYzAwMTVjYzBkZDYiLCJpYXQiOjE3MjE5ODA2ODAsImV4cCI6MTcyMzE5MDI4MH0.BM-Bffn7x4mrYJT06TzOTGpAjF0ItIO21j7f9WF574Q",
+        },
+    })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Qualcosa è andato storto");
+            }
+        })
+        .then((data) => {
+            console.log(data);
+            data.forEach((element, index) => {
+                if (index === 0) {
+                    carousel.innerHTML += `
+                <div class="carousel-item active">
+                    <img src="${element.imageUrl}" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-sm-block">
+                            <h5 class="text-dark">${element.name}</h5>
+                        </div>
+                    </div>`;
+                } else {
+                    carousel.innerHTML += `
+                <div class="carousel-item">
+                    <img src="${element.imageUrl}" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-sm-block">
+                            <h5 class="text-dark">${element.name}</h5>
+                        </div>
+                    </div>`;
+                }
+            });
+
+            btnSelect.addEventListener("click", function () {
+                let active = document.querySelector("div.active");
+                const imgSelected = active.firstElementChild;
+                const src = imgSelected.getAttribute("src");
+                let dataSelected
+                for (let i = 0; i < data.length; i++) {
+                    if(data[i].imageUrl === src) {
+                        dataSelected = data[i]
+                        console.log(dataSelected)
+                    }
+                }
+                const nameInput1 = document.getElementById("name1");
+                const descriptionInput1 = document.getElementById("description1");
+                const priceInput1 = document.getElementById("price1");
+                const brandInput1 = document.getElementById("brand1");
+                const imgUrlInput1 = document.getElementById("imageUrl1");
+                nameInput1.value = dataSelected.name;
+                descriptionInput1.value = dataSelected.description;
+                priceInput1.value = dataSelected.price;
+                brandInput1.value = dataSelected.brand;
+                imgUrlInput1.value = dataSelected.imageUrl;
+                let id = dataSelected._id;
+                dataSelected = ''
+                btnModify.addEventListener("click", function (e) {
+                    e.preventDefault();                    
+                    if(id) {
+                        const newProduct = new myProduct(
+                            nameInput1.value,
+                            descriptionInput1.value,
+                            brandInput1.value,
+                            imgUrlInput1.value,
+                            priceInput1.value
+                        );
+                        fetch(URL + id, {
+                            method: "PUT",
+                            body: JSON.stringify(newProduct),
+                            headers: {
+                                Authorization:
+                                    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNTcwOGYyNjBjYzAwMTVjYzBkZDYiLCJpYXQiOjE3MjE5ODA2ODAsImV4cCI6MTcyMzE5MDI4MH0.BM-Bffn7x4mrYJT06TzOTGpAjF0ItIO21j7f9WF574Q",
+                                "Content-Type": "application/json",
+                            },
+                        })
+                        .then((response) => {
+                            if (response.ok) {
+                                alert("PRODUCT MODIFIED!");
+                                response.json()                                   
+                                
+                            } else {
+                                alert("ERROR!");
+                                throw new Error("Error in saving the product");
+                            }
+                        })
+                        .then((data) => {
+                            const modifyForm = document.getElementById('modifyForm')
+                            modifyForm.reset()
+                            id = ''
+                            carousel.innerHTML = ' '
+                            modal()
+                        })
+                        .catch((err) => {
+                            console.log("ERROR", err);
+                        });
+                    } 
+                    
+                });
+                btnDelete.addEventListener("click", function (e) {
+                    e.preventDefault();                    
+                    if(id) {
+                        fetch(URL + id, {
+                            method: "DELETE",
+                            headers: {
+                                Authorization:
+                                    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNTcwOGYyNjBjYzAwMTVjYzBkZDYiLCJpYXQiOjE3MjE5ODA2ODAsImV4cCI6MTcyMzE5MDI4MH0.BM-Bffn7x4mrYJT06TzOTGpAjF0ItIO21j7f9WF574Q",
+                            },
+                        })
+                        .then((response) => {
+                            if (response.ok) {
+                                alert("PRODUCT DELETED!");
+                                response.json()                                   
+                                
+                            } else {
+                                alert("ERROR!");
+                                throw new Error("Error in deleting the product");
+                            }
+                        })
+                        .then((data) => {
+                            const modifyForm = document.getElementById('modifyForm')
+                            modifyForm.reset()
+                            id = ''
+                            carousel.innerHTML = ' '
+                            modal()
+                        })
+                        .catch((err) => {
+                            console.log("ERROR", err);
+                        });
+                    } 
+                    
+                });
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+modal();
---
+++ assets/js/index.js
@@ -0,0 +1,100 @@
+const articlesRow = document.getElementById('articlesRow')
+const URL = "https://striveschool-api.herokuapp.com/api/product/"
+let carrello = JSON.parse(localStorage.getItem('cart'))
+let cart = document.getElementById('cart')
+
+
+const createCart = function(array) {
+    cart.innerHTML = ''
+    for (let j = 0; j < array.length; j++) {
+        const divImg = document.createElement('div')
+        divImg.classList.add('col-2')
+        const img = document.createElement('img')
+        img.setAttribute('src', array[j].imageUrl)
+        img.classList.add('w-100')
+        const divText = document.createElement('div')
+        divText.classList.add('col-8')
+        const title = document.createElement('h6')
+        title.innerText = array[j].name
+        const price = document.createElement('p')
+        price.innerText = `${array[j].price} $`
+        const divButton = document.createElement('div')
+        divButton.classList.add('col-2')
+        const btnErase = document.createElement('button')
+        btnErase.classList.add('btn', 'btn-danger')
+        btnErase.innerText = 'X'
+        btnErase.addEventListener('click', function() {
+            array[j].count = 1
+            array.splice(j, 1)
+            cart.innerHTML = ''
+            createCart(array)
+            localStorage.setItem('cart', JSON.stringify(array))
+        })
+        divButton.appendChild(btnErase)
+        divText.appendChild(title)
+        divText.appendChild(price)
+        divImg.appendChild(img)
+        cart.appendChild(divImg)
+        cart.appendChild(divText)
+        cart.appendChild(divButton)
+    }    
+    let total = array.reduce(function(acc, element) {
+        return acc + element.price
+    }, 0)
+    const totalShow = document.createElement('h5')
+    totalShow.innerText = `Il totale nel carrello è: ${total.toFixed(2)} $`
+    cart.appendChild(totalShow)
+
+    
+    
+}
+
+if (carrello) {
+    createCart(carrello)
+} else {
+    carrello = []
+}
+const retrieveArticles = function () {
+    fetch(URL, {
+        headers: {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNTcwOGYyNjBjYzAwMTVjYzBkZDYiLCJpYXQiOjE3MjE5ODA2ODAsImV4cCI6MTcyMzE5MDI4MH0.BM-Bffn7x4mrYJT06TzOTGpAjF0ItIO21j7f9WF574Q"
+        }
+    })
+    .then(response => {
+        if(response.ok) {
+           return response.json()
+        } else {
+            throw new Error ('Qualcosa è andato storto')
+        }
+    })
+    .then(data => {
+        console.log(data)
+        data.forEach((element) => {
+            let description = ''
+            if (element.description.length > 50) {
+                description = element.description.slice(0, element.description.indexOf(" ", 50))
+                description += ' ... '
+            } else {
+                description = element.description
+            }
+            const newProductCol = `
+            <div class="col">
+                <div class="card">
+                    <img src="${element.imageUrl}" class="card-img-top" alt="..." height='250' style='width: 250pxs !important'>
+                    <div class="card-body text-center">
+                        <h5 class="card-title">${element.name}</h5>
+                        <h6 class="card-title">${element.brand}</h6>
+                        <p class="card-text">${description}</p>
+                        <a href="details.html?productId=${element._id}" class="btn btn-primary w-50">DETAILS</a>
+                    </div>
+                </div>
+            </div> `;
+            articlesRow.innerHTML += newProductCol
+        });
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+retrieveArticles()
